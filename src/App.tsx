419365198cfd25a1e142a70a83d89db1c755d1f0
Adnen Rebai <adnen.rebai@gmail.com>
2025-04-10 12:47:44 +0200

    add themeSwitcher and enchange app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import LetterSumEditor from "./components/molecules/LetterSumEditor";
+import ThemeSwitcher from "./components/molecules/ThemeSwitcher";
 
 function App() {
   return (
     <>
+      <ThemeSwitcher />
       <LetterSumEditor />
     </>
   );
--- src/components/molecules/LetterSumEditor.tsx
+++ src/components/molecules/LetterSumEditor.tsx
@@ -3,8 +3,9 @@ import { useState, useRef } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
+import { FaSun, FaMoon } from "react-icons/fa";
 
-const arabicValues: Record<string, number> = {
+const arabicValuesSun = {
   ا: 1,
   ب: 2,
   ج: 3,
@@ -34,6 +35,37 @@ const arabicValues: Record<string, number> = {
   ظ: 900,
   غ: 1000,
 };
+const arabicValuesMoon = {
+  ا: 13,
+  ب: 0,
+  ج: 0,
+  د: 0,
+  ه: 2,
+  و: 0,
+  ز: 0,
+  ح: 7,
+  ط: 4,
+  ي: 2,
+  ك: 1,
+  ل: 13,
+  م: 17,
+  ن: 1,
+  س: 5,
+  ع: 2,
+  ف: 0,
+  ص: 3,
+  ق: 2,
+  ر: 6,
+  ش: 0,
+  ت: 0,
+  ث: 0,
+  خ: 0,
+  ذ: 0,
+  ض: 0,
+  ظ: 0,
+  غ: 0,
+};
+let arabicValues: Record<string, number> = arabicValuesSun;
 
 const getFrenchSum = (text: string) => {
   return text
@@ -43,17 +75,23 @@ const getFrenchSum = (text: string) => {
     .reduce((sum, char) => sum + (char.charCodeAt(0) - 64), 0);
 };
 
-const getArabicSum = (text: string) => {
-  return text
-    .split("")
-    .filter((char) => arabicValues[char])
-    .reduce((sum, char) => sum + arabicValues[char], 0);
+const reduceToOneDigit = (num: number): number => {
+  while (num >= 10) {
+    num = num
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((acc, digit) => acc + digit, 0);
+  }
+  return num;
 };
 
 export default function LetterSumEditor() {
   const [text, setText] = useState("");
   const [isArabic, setIsArabic] = useState(false);
+  const [isArSun, setisArSun] = useState(true);
   const inputRef = useRef<HTMLTextAreaElement>(null);
+
   const handleLanguageSwitch = () => {
     // Toggle language between Arabic and French
     setIsArabic(!isArabic);
@@ -61,7 +99,16 @@ export default function LetterSumEditor() {
     // Focus on the input after switching the language
     inputRef.current?.focus();
   };
-
+  const handleArLanguageSwitch = () => {
+    setisArSun(!isArSun);
+  };
+  const getArabicSum = (text: string) => {
+    arabicValues = isArSun ? arabicValuesSun : arabicValuesMoon;
+    return text
+      .split("")
+      .filter((char) => arabicValues[char])
+      .reduce((sum, char) => sum + arabicValues[char], 0);
+  };
   const sum = isArabic ? getArabicSum(text) : getFrenchSum(text);
 
   return (
@@ -76,6 +123,23 @@ export default function LetterSumEditor() {
           />
           <Label>{"Arabic"}</Label>
         </div>
+        {/* Sun/Moon Theme Toggle */}
+        {isArabic ? (
+          <div className="flex items-center justify-center space-x-4">
+            <Label htmlFor="lang-switch">
+              <FaSun className="text-yellow-500" />
+            </Label>
+            <Switch
+              id="arabic-switch"
+              checked={!isArSun}
+              onCheckedChange={handleArLanguageSwitch}
+            />
+            <Label>
+              <FaMoon className="text-gray-300" />
+            </Label>
+          </div>
+        ) : null}
+
         <Textarea
           ref={inputRef}
           placeholder={isArabic ? "أدخل النص هنا" : "Enter text here"}
@@ -87,6 +151,9 @@ export default function LetterSumEditor() {
         <div className="text-xl font-semibold">
           {isArabic ? "المجموع" : "Sum"}:{" "}
           <span className="text-primary">{sum}</span>
+        </div>
+        <div className="text-xl font-semibold">
+          <span className="text-primary">{reduceToOneDigit(sum)}</span>
         </div>
       </div>
     </div>
