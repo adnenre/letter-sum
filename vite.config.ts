f45f0189ecb2ad47e9c838d448ce79e5669d284d
Adnen rebai <adnen.rebai@gmail.com>
2025-06-27 21:28:28 +0100

    Update vite.config.ts
    Update base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/letter-sum-editor",
+  base: "/letter-sum",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
